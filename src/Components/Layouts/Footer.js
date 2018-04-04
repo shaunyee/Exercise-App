74d753603569885d9172282215835abae9113d73
Shaun Yee <yeezy@Shauns-MacBook-Pro.local>
2018-04-03 19:08:48 -0700

    mapped over list of exercises and displayed them in left pane

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Paper, Tabs } from 'material-ui';
 import { Tab } from 'material-ui/Tabs';
 
-export default props =>
+export default ({ muscles }) =>
   <Paper>
     <Tabs
       value={0}
@@ -10,8 +10,9 @@ export default props =>
       textColor="primary"
       centered
     >
-      <Tab label="Item One" />
-      <Tab label="Item Two" />
-      <Tab label="Item Three" />
+      <Tab label="All" />
+      {muscles.map(group =>
+        <Tab label={group} />
+      )}
     </Tabs>
   </Paper>
